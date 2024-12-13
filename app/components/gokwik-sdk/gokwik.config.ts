78d09d48753567892732122bc0c2964029ec89d2
Lakshya Wadhwa <lakshya@gokwik.co>
2024-12-13 20:20:27 +0530

    fix: test cred

--- app/components/gokwik-sdk/gokwik.config.ts
+++ app/components/gokwik-sdk/gokwik.config.ts
@@ -12,11 +12,11 @@ shopifyGraphQlUrl: Graph QL API for your store, follows the format-
 
 export const gokwikConfig = {
   mid: '19g6ilb0pbcu7',
-  storefrontAccessToken: '7bd989f4ef8935305413a01a0bbabf5b',
-  shopifyGraphQlUrl:
-    'https://qa-store-kwik-labs.myshopify.com/api/2022-07/graphql.json',
-  gkShopDomain: 'qa-store-kwik-labs.myshopify.com',
+  storefrontAccessToken: '1e4b402554a38216e446f1607a61c3dc',
+  shopifyGraphQlUrl: 'https://wsyk17-s0.myshopify.com/api/2022-07/graphql.json',
+  gkShopDomain: 'wsyk17-s0.myshopify.com',
 };
+//https://sa-hydrogen-storefront-61e6fa1690867543bfae.o2.myshopify.dev/products/diljit-dosanjh-dil-uminati-tour-merch-1?Size=XS
 
 // window.gkCustomerEmail = "{{ customer.email }}";
 // window.gkLogOutUrl = "{{ routes.account_logout_url }}";
